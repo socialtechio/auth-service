74858bf7b25365bd8f2cc5b681c2b6ba585936cd
vladislavverpeta <vladverpeta@gmail.com>
2018-09-07 17:58:43 +0300

    Updated docs

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ REQUEST_COUNT=25
 
 docker run --rm --net=host --entrypoint=/bin/sh jordi/ab \
 "-c" \
-"if timeout -t ${TIMEOUT_SEC} ab -n ${REQUEST_COUNT} -c ${REQUEST_CONCURRENCY} http://localhost:80/; then echo \"Test successful, your code now as 🚀🚀🚀\"; else echo \"Too slow 🐌🐌🐌. Test Failed\"; fi"
+"if timeout -t ${TIMEOUT_SEC} ab -n ${REQUEST_COUNT} -c ${REQUEST_CONCURRENCY} http://localhost:80/uploadDataURL?someVar=someValue; then echo \"Test successful, your code now as 🚀🚀🚀\"; else echo \"Too slow 🐌🐌🐌. Test Failed\"; fi"
